3bef7206a4167a5a02d9d53a5286a0ddf34df7d3
Tze Kheng Goh <dgoh03625@yahoo.com>
2025-09-19 20:49:13 +1000

    Added all features for an interview and added some styling for pages. Now can begin working on add button and screens for questions and applicants.

--- ready-set-hire/src/components/InterviewList.jsx
+++ ready-set-hire/src/components/InterviewList.jsx
@@ -1,10 +1,24 @@
 
 import { Link, useLoaderData } from "react-router-dom";
-import { getAllInterviews } from "../apis/interviewapi";
+import { deleteInterview, getAllInterviews } from "../apis/interviewapi";
+import { getAllQuestions } from "../apis/questionsapi"
+import { getAllApplicants } from "../apis/applicantapi"
 
 export async function loader({ request }) {
-  const posts = await getAllInterviews({ signal: request.signal });
-  return { posts };
+  const allinterviews = await getAllInterviews({ signal: request.signal });
+  // using map as every element has a call back in map.
+  const interviewsMetadata = await Promise.all(    // returns an array of unresolved promises. When resolved it is interview project with 
+      allinterviews.map(async (interview) => {     // question and applicant count. 
+        const questions = await getAllQuestions(interview.id, { signal: request.signal });
+        const applicants = await getAllApplicants(interview.id, { signal: request.signal });
+
+        return { ...interview,   // returns value that goes into array, then goes to next callback
+                numQ: questions.length,
+                numApp: applicants.length,
+        };
+      })
+  );
+  return { interviewsMetadata };
 }
 
 function getStatusBadgeClass(status) {
@@ -19,7 +33,7 @@ function getStatusBadgeClass(status) {
 }
 
 export default function InterviewList() {
-  const { posts } = useLoaderData();
+  const { interviewsMetadata: interviews } = useLoaderData();
 
   return (
     <div className="container mt-4">
@@ -29,33 +43,68 @@ export default function InterviewList() {
       </p>
 
       <div className="accordion" id="interviewAccordion">
-        {posts.map((post, index) => (
-          <div className="accordion-item" key={post.id}>
-            <h2 className="accordion-header align-items-center" id={`heading${post.id}`}>
-              <button
+        {interviews.map((interview, index) => (
+          <div className="accordion-item" key={interview.id}>
+            <h2 className="accordion-header align-items-center" id={`heading${interview.id}`}>
+            <div className="d-flex align-items-center justify-content-between w-100">
+            <button
                 className="accordion-button collapsed flex-grow-1 text-start"
                 type="button"
                 data-bs-toggle="collapse"
-                data-bs-target={`#collapse${post.id}`}
+                data-bs-target={`#collapse${interview.id}`}
                 aria-expanded="false"
-                aria-controls={`collapse${post.id}`}
+                aria-controls={`collapse${interview.id}`}
               >
-                <div className="align-items-center w-100 ">
-                  <span>{post.title}</span>
-                  <span className={`badge ${getStatusBadgeClass(post.status)} ms-2`}>
-                  {post.status}
-                  </span>
+                <div className="align-items-center w-100 justify-content-start">
+                  <span>{interview.title}</span>
+                  <span className={`badge ${getStatusBadgeClass(interview.status)} ms-2`}>
+                      {interview.status}
+                    </span>
                 </div>
               </button>
+               <button
+                     type="button"
+                     className="btn btn-outline-danger btn-sm ms-1 me-2"
+                    onClick={() => 
+                      deleteInterview(interview.id)
+                    }
+                  >
+                    <i className="bi bi-trash-fill"></i>
+                    </button>
+                </div>
             </h2>
             <div
-              id={`collapse${post.id}`}
+              id={`collapse${interview.id}`}
               className="accordion-collapse collapse"
-              aria-labelledby={`heading${post.id}`}
+              aria-labelledby={`heading${interview.id}`}
               data-bs-parent="#interviewAccordion"
             >
-              <div className="accordion-body">
-                <p>{post.description || "No description available."}</p>
+              <div className="accordion-body text-start">
+                <h5 className="mb-3 text-muted">{interview.job_role || "No job role provided"}</h5>
+                <p className="mb-1 fw-semibold">What we're looking for:</p>
+                <p className="mb-3 text-muted">{interview.description || "No description available."}</p>
+             <div className="d-flex gap-2 justify-content-center">
+              <Link
+                  to="/questions"
+                  className="card p-2 align-items-center text-decoration-none text-dark justify-content-center"
+                  style={{ width: "12rem", height: "3rem"}}
+                >
+                  <span className="text-gray-800 text-muted">
+                    <i className="bi bi-question-square me-2"></i> 
+                    Questions: ({interview.numQ})
+                  </span>
+                </Link>
+                <Link
+                  to="/applicants"
+                  className="card p-2 align-items-center text-decoration-none text-dark justify-content-center"
+                  style={{ width: "12rem", height: "3rem"}}
+                >
+                  <span className="text-gray-800 text-muted">
+                    <i className="bi bi-people-fill me-2"></i> 
+                    Applicants: ({interview.numApp})
+                  </span>
+                </Link>
+               </div> 
               </div>
             </div>
           </div>
